edd71c38d0e72cf49ab5009e36bc02a0b68ba72f
vicmon810 <maokr879@student.otago.ac.nz>
2023-11-21 14:58:36 +1300

    solution update

---
+++ Algorithms/Easy/MergeString.java
@@ -0,0 +1,27 @@
+package Algorithms.Easy;
+
+/*
+ * You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+ */
+class MergeString {
+    public static String mergeAlternately(String word1, String word2) {
+        StringBuilder result = new StringBuilder();
+        int i = 0;
+        while (i < word1.length() || i < word2.length()) {
+            if (i < word1.length())
+                result.append(word1.charAt(i));
+            if (i < word2.length())
+                result.append(word2.charAt(i));
+            i++;
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        try {
+            System.out.println(mergeAlternately("hl", "elo"));
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+}
