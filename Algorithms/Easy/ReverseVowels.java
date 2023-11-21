41c78ef71a664ee05aec7eb6cfc4dcb16cf75f12
vicmon810 <maokr879@student.otago.ac.nz>
2023-11-21 13:53:12 +1300

    initial commit

---
+++ Algorithms/Easy/ReverseVowels.java
@@ -0,0 +1,45 @@
+package Algorithms.Easy;
+/*
+Given a string s, reverse only all the vowels in the string and return it.
+The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+ */
+
+class ReverseVowels {
+    public static String reverseVowels(String s) {
+        char[] word = s.toCharArray();
+        int start = 0;
+        int end = s.length() - 1;
+        String vowels = "aeiouAEIOU";
+
+        while (start < end) {
+
+            while (start < end && vowels.indexOf(word[start]) == -1) {
+                start++;
+            }
+
+            while (start < end && vowels.indexOf(word[end]) == -1) {
+                end--;
+            }
+
+            char temp = word[start];
+            word[start] = word[end];
+            word[end] = temp;
+
+            start++;
+            end--;
+        }
+        String ans = new String(word);
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        try {
+            System.out.println(reverseVowels("Leetcode"));
+            System.out.println(reverseVowels("solution"));
+            System.out.println(reverseVowels("hEllO"));
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# LeetCode Solution
+
+## Project overview: 
+This repository is dedicated to storing solutions for LeetCode problems. It includes solutions that I have personally developed as well as those gathered from various online resources. The primary goal of this project is to create a comprehensive reference for LeetCode problem solutions to aid in learning and review.
+
+## Feature:
+- A wide range of solution to LeetCode problems
+- Solution from both personal work and external sources.
+- Organized structure for easy navigation and reference. 
+
