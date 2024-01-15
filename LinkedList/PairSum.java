cf97515fbaac33f806d2dc1251c53e1b92a60a50
vicmon810 <maokr879@student.otago.ac.nz>
2024-01-15 17:00:49 +1300

    pair sum

---
+++ LinkedList/PairSum.java
@@ -0,0 +1,52 @@
+package LinkedList;
+
+public class PairSum {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+
+    public int pairSum(ListNode head) {
+
+        ListNode middle = findMiddle(head);
+        ListNode second = reverse(middle);
+
+        int max = 0;
+        while (head != null && second != null) {
+            max = Math.max(head.val + second.val, max);
+            head = head.next;
+            second = second.next;
+        }
+        return max;
+    }
+
+    public ListNode findMiddle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+}
--- LinkedList/ReviseList.java
+++ LinkedList/ReviseList.java
@@ -1,5 +1,16 @@
 package LinkedList;
 
 public class ReviseList {
-    
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
 }
