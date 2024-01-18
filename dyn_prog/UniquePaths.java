92d07b7fdf8e84dd645f936837af78b54572f484
vicmon810 <maokr879@student.otago.ac.nz>
2024-01-18 15:27:44 +1300

    update version

--- dyn_prog/UniquePaths.java
+++ dyn_prog/UniquePaths.java
@@ -3,18 +3,24 @@ package dyn_prog;
 public class UniquePaths {
 
     public static int uniquePaths(int m, int n) {
-        return uniquePath(m - 1, n - 1);
-    }
+        Integer[][] map = new Integer[m][n];
+        for (int i = 0; i < m; i++) {
+            map[i][0] = 1;
+        }
 
-    static int uniquePath(int m, int n) {
-        if (m < 0 || n < 0)
-            return 0;
-        else if (m == 0 || m == 0)
-            return 1;
-        return uniquePath(m - 1, n) + uniquePath(m, n - 1);
+        for (int j = 0; j < n; j++) {
+            map[0][j] = 1;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                map[i][j] = map[i - 1][j] + map[i][j - 1];
+            }
+        }
+        return map[m - 1][n - 1];
     }
 
     public static void main(String[] args) {
-        System.out.println(uniquePath(3, 7));
+        System.out.println(uniquePaths(3, 7));
     }
 }
