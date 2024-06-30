9448ba34f498603413e264f987ff919fa461dc1b
vicmon810 <m384572038@gmail.com>
2024-06-30 14:54:51 +1200

    greatest comment string

---
+++ easy_solutions/gdcString.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def gcdOfStrings(self, str1, str2):
+        """
+        :type str1: str
+        :type str2: str
+        :rtype: str
+        """
+        if str1 + str2 != str2 + str1: 
+            return ""
+        
+        if len(str1) == len(str2):
+            return str1
+        
+        if len(str1) > len(str2):
+            return self.gcdOfStrings(str1[len(str2):], str2)
+
+        return self.gcdOfStrings(str1, str2[len(str1):]) 
