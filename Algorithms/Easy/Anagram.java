af388c437fee4307bae231071b4066e068bf6aec
Kris <100879814+vicmon810@users.noreply.github.com>
2024-07-18 19:25:35 +1200

    Update Anagram.java

--- Algorithms/Easy/Anagram.java
+++ Algorithms/Easy/Anagram.java
@@ -3,7 +3,7 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         char[] sChars = s.toCharArray();
         char[] tChars = t.toCharArray();
-
+1231
         
         Arrays.sort(sChars);
         Arrays.sort(tChars);
